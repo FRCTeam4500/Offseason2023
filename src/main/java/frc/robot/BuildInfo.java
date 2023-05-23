7eb67f1443f04ba6336a678da8b6c7a796f5e4fc
Sal <spoovathumkadavil16@claytonschools.net>
2023-05-22 19:02:40 -0500

    Reset swerveRewrite To swerveCommand Hotfix

---
+++ src/main/java/frc/robot/BuildInfo.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildInfo {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "Offseason2023";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 50;
+    public static final String GIT_SHA = "bd432f4aa4efd419863b19445bc866542a1a6114";
+    public static final String GIT_DATE = "2023-05-20 21:32:45 CDT";
+    public static final String GIT_BRANCH = "swerveRewrite";
+    public static final String BUILD_DATE = "2023-05-22 09:03:38 CDT";
+    public static final long BUILD_UNIX_TIME = 1684764218086L;
+    public static final int DIRTY = 1;
+
+    private BuildInfo(){}
+}
