b5bbcaad0388a5616ac83699e53f08e6301f47c7
ChiefLeef <97705117+SPoovathumkadavil@users.noreply.github.com>
2023-05-24 15:14:16 -0500

    SwerveModuleLogging now works

--- src/main/java/frc/robot/utility/LogSubsystemInputsTask.java
+++ src/main/java/frc/robot/utility/LogSubsystemInputsTask.java
@@ -6,6 +6,8 @@ import frc.robot.subsystem.swerve.SwerveDrive;
 import java.util.TimerTask;
 import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
 public class LogSubsystemInputsTask extends TimerTask {
 
 	Arm arm;
@@ -28,8 +30,9 @@ public class LogSubsystemInputsTask extends TimerTask {
 		Logger.getInstance().processInputs("Intake", intake.getInputs());
 		Logger.getInstance().processInputs("Swerve", swerve.getInputs());
 		Logger.getInstance().recordOutput("Odometry", swerve.getRobotPose());
+		SwerveModuleState[] states = swerve.getModuleStates();
 		Logger
 			.getInstance()
-			.recordOutput("Swerve Modules", swerve.getModuleStates());
+			.recordOutput("ModuleStates", states[0], states[1], states[2], states[3]);
 	}
 }
