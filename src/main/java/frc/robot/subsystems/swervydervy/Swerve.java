47ce36aa18ebff7fb9b4340fe5ca074a0a18490c
Saaleh <97705117+SPoovathumkadavil@users.noreply.github.com>
2023-10-01 20:44:15 -0500

    swervy dwervy :D

---
+++ src/main/java/frc/robot/subsystems/swervydervy/Swerve.java
@@ -0,0 +1,343 @@
+package frc.robot.subsystems.swervydervy;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.I2C.Port;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.SwerveConstants;
+import frc.robot.hardware.Gyro;
+import frc.robot.hardware.TalonMotorController;
+
+public class Swerve extends SubsystemBase {
+
+    private final SwerveModule[] swerveModules;
+    private final SwerveDriveKinematics kinematics;
+    private OdematicsStrat odometry;
+    private final Gyro gyro;
+    private final double maxSpeedMetersPerSecond;
+    private Rotation2d gyroOffset = new Rotation2d();
+    private boolean hasGyroOffset = false;
+
+    private static Swerve instance = null;
+
+    /**
+     * Construct a swerve drive object. Along with a gyro, this takes in four
+     * configured swerve
+     * modules, by convention in left front, right front, left rear, right rear
+     * order.
+     *
+     * @param gyro          the gyro to use for field-centric driving
+     * @param swerveModules the swerve modules
+     */
+    private Swerve(Gyro gyro, SwerveModule... swerveModules) {
+        this.gyro = gyro;
+        this.swerveModules = swerveModules;
+
+        /*
+         * final List<Translation2d> locations = Arrays.stream(swerveModules)
+         * .map(SwerveModule::getWheelLocationMeters)
+         * .collect(Collectors.toList());
+         */
+
+        Translation2d[] translation2ds = Arrays.stream(swerveModules)
+                .map(SwerveModule::getWheelLocationMeters)
+                .toArray(Translation2d[]::new);
+
+        // verify all swerve modules are set to same max speed
+        Set<Double> maxSpeeds = Arrays.stream(swerveModules)
+                .map(SwerveModule::getMaxSpeedMetersPerSecond)
+                .collect(Collectors.toSet());
+
+        if (maxSpeeds.size() > 1) {
+            throw new IllegalStateException("swerve modules must have same driveMaximumMetersPerSecond");
+        }
+        maxSpeedMetersPerSecond = swerveModules[0].getMaxSpeedMetersPerSecond();
+
+        SwerveModulePosition[] modulePositions = Arrays.stream(swerveModules)
+                .map(SwerveModule::getPosition)
+                .toArray(SwerveModulePosition[]::new);
+
+        kinematics = new SwerveDriveKinematics(translation2ds);
+        odometry = new OdematicsStrat(
+                kinematics, gyro.getRotation2d().rotateBy(gyroOffset), modulePositions);
+    }
+
+    /**
+     * Construct a swerve drive object with a navX gyro. This takes in four
+     * configured swerve modules,
+     * by convention in left front, right front, left rear, right rear order.
+     *
+     * @param swerveModules the swerve modules
+     */
+    private Swerve(SwerveModule... swerveModules) {
+        this(new Gyro(Port.kMXP), swerveModules);
+    }
+
+    public static synchronized Swerve getInstance() {
+        if (instance == null) {
+            instance = new Swerve(
+                    new SwerveModule(
+                            new TalonMotorController(SwerveConstants.DFLPORT, "Talon FX"),
+                            new TalonMotorController(SwerveConstants.AFLPORT, "Talon FX"),
+                            SwerveConstants.FRONT_LEFT_MODULE_TRANSLATION),
+                    new SwerveModule(
+                            new TalonMotorController(SwerveConstants.DFRPORT, "Talon FX"),
+                            new TalonMotorController(SwerveConstants.AFRPORT, "Talon FX"),
+                            SwerveConstants.FRONT_RIGHT_MODULE_TRANSLATION),
+                    new SwerveModule(
+                            new TalonMotorController(SwerveConstants.DBLPORT, "Talon FX"),
+                            new TalonMotorController(SwerveConstants.ABLPORT, "Talon FX"),
+                            SwerveConstants.BACK_LEFT_MODULE_TRANSLATION),
+                    new SwerveModule(
+                            new TalonMotorController(SwerveConstants.DBRPORT, "Talon FX"),
+                            new TalonMotorController(SwerveConstants.ABRPORT, "Talon FX"),
+                            SwerveConstants.BACK_RIGHT_MODULE_TRANSLATION));
+        }
+        return instance;
+    }
+
+    /**
+     * Replace the default {@code OdematicsStrat} with another such as
+     * {@code
+     * EstimatorStrat}. This is provided as a setter so that the
+     * current configured
+     * {@code SwerveDriveKinematics} can be used in constructing the
+     * {@code OdematicsStrat}.
+     *
+     * <p>
+     * <strong>IMPORTANT:</strong> This should be called right after the constructor
+     * and before any
+     * use of the drive.
+     *
+     * @param odometry the replacement odometry calculation method
+     */
+    public void setOdometry(OdematicsStrat odometry) {
+        this.odometry = odometry;
+    }
+
+    /**
+     * Returns the kinematics object in use by this swerve drive.
+     *
+     * @return The kinematics object in use.
+     */
+    public SwerveDriveKinematics getKinematics() {
+        return kinematics;
+    }
+
+    /**
+     * Returns the position of the robot on the field.
+     *
+     * @return the pose of the robot (x and y are in meters)
+     */
+    public Pose2d getPoseMeters() {
+        return odometry.getPoseMeters();
+    }
+
+    /**
+     * Returns the current gyro-measured heading of the robot. This will be affected
+     * by any gyro drift
+     * that may have accumulated since last gyro recalibration. The angle is
+     * continuous, that is it
+     * will continue from 360 to 361 degrees. This allows algorithms that wouldn't
+     * want to see a
+     * discontinuity in the gyro output as it sweeps past from 360 to 0 on the
+     * second time around. The
+     * angle is expected to increase as the gyro turns counterclockwise when looked
+     * at from the top.
+     *
+     * @return the Rotation2d of the robot relative to gyro zero
+     */
+    public Rotation2d getHeading() {
+        return hasGyroOffset ? gyro.getRotation2d().rotateBy(gyroOffset) : gyro.getRotation2d();
+    }
+
+    /**
+     * Returns the current gyro-measured heading of the robot. This will be affected
+     * by any gyro drift
+     * that may have accumulated since last gyro recalibration. The angle is
+     * continuous, that is it
+     * will continue from 360 to 361 degrees. This allows algorithms that wouldn't
+     * want to see a
+     * discontinuity in the gyro output as it sweeps past from 360 to 0 on the
+     * second time around. The
+     * angle is expected to increase as the gyro turns clockwise when looked at from
+     * the top.
+     *
+     * @return the current heading in degrees of the robot relative to gyro zero
+     */
+    double getGyroAngle() {
+        // FIXME: does not have gyro offset
+        return gyro.getAngle();
+    }
+
+    /**
+     * Return the rate of rotation of the gyro. The rate is based on the most recent
+     * reading of the
+     * gyro analog value. The rate is expected to be positive as the gyro turns
+     * clockwise when looked
+     * at from the top.
+     *
+     * @return the current rate in degrees per second
+     */
+    public double getGyroRate() {
+        return gyro.getRate();
+    }
+
+    /**
+     * Get the current gyro offset applied to the IMU gyro angle during field
+     * oriented driving.
+     *
+     * @return the gyro offset
+     */
+    public Rotation2d getGyroOffset() {
+        return gyroOffset;
+    }
+
+    /**
+     * Set the current gyro offset applied to the IMU gyro angle during field
+     * oriented driving,
+     * defaults to zero.
+     *
+     * @param gyroOffset the desired offset
+     */
+    public void setGyroOffset(Rotation2d gyroOffset) {
+        if (this.gyroOffset.equals(gyroOffset)) {
+            return;
+        }
+        this.gyroOffset = gyroOffset;
+        hasGyroOffset = true;
+    }
+
+    /**
+     * Get the configured swerve modules.
+     *
+     * @return array of swerve modules
+     */
+    public SwerveModule[] getSwerveModules() {
+        return swerveModules;
+    }
+
+    /**
+     * Resets the robot's position on the field. Any accumulated gyro drift will be
+     * noted and
+     * accounted for in subsequent calls to {@link #getPoseMeters()}.
+     *
+     * @param pose The robot's actual position on the field.
+     */
+    public void resetOdometry(Pose2d pose) {
+        odometry.resetPosition(
+                pose,
+                gyro.getRotation2d().rotateBy(gyroOffset),
+                swerveModules[0].getPosition(),
+                swerveModules[1].getPosition(),
+                swerveModules[2].getPosition(),
+                swerveModules[3].getPosition());
+    }
+
+    /** Resets the drive encoders to currently read a position of 0. */
+    public void resetDriveEncoders() {
+        for (int i = 0; i < 4; i++) {
+            swerveModules[i].resetDriveEncoder();
+        }
+    }
+
+    /** Resets the gyro to a heading of zero. */
+    public void resetGyro() {
+        gyro.reset();
+    }
+
+    /**
+     * Update the swerve drive odometry state. Call this from the drive subsystem
+     * {@code periodic()}
+     * method.
+     */
+    public void periodic() {
+        odometry.update(
+                hasGyroOffset ? gyro.getRotation2d().rotateBy(gyroOffset) : gyro.getRotation2d(),
+                swerveModules[0].getPosition(),
+                swerveModules[1].getPosition(),
+                swerveModules[2].getPosition(),
+                swerveModules[3].getPosition());
+    }
+
+    /**
+     * Drive the robot with given x, y, and rotational velocities using open-loop
+     * velocity control.
+     *
+     * @param vxMetersPerSecond     the desired x velocity component
+     * @param vyMetersPerSecond     the desired y velocity component
+     * @param omegaRadiansPerSecond the desired rotational velocity component
+     * @param isFieldOriented       true if driving field-oriented
+     */
+    public void drive(
+            double vxMetersPerSecond,
+            double vyMetersPerSecond,
+            double omegaRadiansPerSecond,
+            boolean isFieldOriented) {
+        SwerveModuleState[] swerveModuleStates = getSwerveModuleStates(
+                vxMetersPerSecond, vyMetersPerSecond, omegaRadiansPerSecond, isFieldOriented);
+        for (int i = 0; i < 4; i++) {
+            swerveModules[i].setDesiredState(swerveModuleStates[i], true);
+        }
+    }
+
+    /**
+     * Move the robot with given x, y, and rotational velocities using closed-loop
+     * velocity control.
+     *
+     * @param vxMetersPerSecond     the desired x velocity component
+     * @param vyMetersPerSecond     the desired y velocity component
+     * @param omegaRadiansPerSecond the desired rotational velocity component
+     * @param isFieldOriented       true if driving field-oriented
+     */
+    public void move(
+            double vxMetersPerSecond,
+            double vyMetersPerSecond,
+            double omegaRadiansPerSecond,
+            boolean isFieldOriented) {
+        SwerveModuleState[] swerveModuleStates = getSwerveModuleStates(
+                vxMetersPerSecond, vyMetersPerSecond, omegaRadiansPerSecond, isFieldOriented);
+        for (int i = 0; i < 4; i++) {
+            swerveModules[i].setDesiredState(swerveModuleStates[i], false);
+        }
+    }
+
+    private SwerveModuleState[] getSwerveModuleStates(
+            double vxMetersPerSecond,
+            double vyMetersPerSecond,
+            double omegaRadiansPerSecond,
+            boolean isFieldOriented) {
+        ChassisSpeeds chassisSpeeds = isFieldOriented
+                ? ChassisSpeeds.fromFieldRelativeSpeeds(
+                        vxMetersPerSecond,
+                        vyMetersPerSecond,
+                        omegaRadiansPerSecond,
+                        hasGyroOffset ? gyro.getRotation2d().rotateBy(gyroOffset) : gyro.getRotation2d())
+                : new ChassisSpeeds(vxMetersPerSecond, vyMetersPerSecond, omegaRadiansPerSecond);
+
+        var swerveModuleStates = kinematics.toSwerveModuleStates(chassisSpeeds);
+        SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, maxSpeedMetersPerSecond);
+        return swerveModuleStates;
+    }
+
+    /**
+     * Directly set the swerve modules to the specified states.
+     *
+     * @param desiredStates the desired swerve module states
+     */
+    public void setModuleStates(SwerveModuleState[] desiredStates) {
+        SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, maxSpeedMetersPerSecond);
+        for (int i = 0; i < 4; i++) {
+            swerveModules[i].setDesiredState(desiredStates[i], true);
+        }
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/swervydervy/SwerveModule.java
@@ -0,0 +1,165 @@
+package frc.robot.subsystems.swervydervy;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.BaseTalon;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
+import frc.robot.hardware.TalonMotorController;
+
+public class SwerveModule {
+    final int k100msPerSecond = 10;
+
+    private final TalonMotorController angleMotor;
+    private final TalonMotorController driveMotor;
+    private final double angleCountsPerRev;
+    private final double driveCountsPerRev;
+    private final double driveGearRatio;
+    private final double wheelCircumferenceMeters;
+    private final double driveDeadbandMetersPerSecond;
+    private final double driveMaximumMetersPerSecond;
+    private final Translation2d wheelLocationMeters; // forward pos x, left pos y
+
+    private Rotation2d previousAngle = new Rotation2d();
+
+    public SwerveModule(TalonMotorController driveMotor, TalonMotorController angleMotor,
+            Translation2d wheelLocationMeters) {
+        this.angleMotor = angleMotor;
+        this.driveMotor = driveMotor;
+        angleCountsPerRev = 2048; // Should be for FX
+        driveCountsPerRev = 2048;
+        driveGearRatio = 5.;
+        wheelCircumferenceMeters = Math.PI * Units.inchesToMeters(3);
+        driveDeadbandMetersPerSecond = 0.05;
+        driveMaximumMetersPerSecond = 5.;
+        this.wheelLocationMeters = wheelLocationMeters;
+        resetDriveEncoder();
+    }
+
+    public double getMaxSpeedMetersPerSecond() {
+        return driveMaximumMetersPerSecond;
+    }
+
+    public Translation2d getWheelLocationMeters() {
+        return wheelLocationMeters;
+    }
+
+    public double getDriveCountsPerRev() {
+        return driveCountsPerRev;
+    }
+
+    public SwerveModuleState getState() {
+        double speedMetersPerSecond = getDriveMetersPerSecond();
+        Rotation2d angle = getAngleMotorRotation2d();
+        return new SwerveModuleState(speedMetersPerSecond, angle);
+    }
+
+    public SwerveModulePosition getPosition() {
+        double wheelPositionMeters = getDrivePositionMeters();
+        Rotation2d angle = getAngleMotorRotation2d();
+        return new SwerveModulePosition(wheelPositionMeters, angle);
+    }
+
+    public void setDesiredState(SwerveModuleState desiredState, boolean isDriveOpenLoop) {
+        assert desiredState.speedMetersPerSecond >= 0.0;
+
+        if (desiredState.speedMetersPerSecond < driveDeadbandMetersPerSecond) {
+            desiredState = new SwerveModuleState(0.0, previousAngle);
+        }
+
+        SwerveModuleState optimizedState = setAngleOptimizedState(desiredState);
+
+        if (isDriveOpenLoop) {
+            setDriveOpenLoopMetersPerSecond(optimizedState.speedMetersPerSecond);
+        } else {
+            setDriveClosedLoopMetersPerSecond(optimizedState.speedMetersPerSecond);
+        }
+    }
+
+    public void resetDriveEncoder() {
+        driveMotor.setSelectedSensorPosition(0);
+    }
+
+    public TalonMotorController getAngleMotor() {
+        return angleMotor;
+    }
+
+    public BaseTalon getDriveMotor() {
+        return driveMotor;
+    }
+
+    public Rotation2d getAngleMotorRotation2d() {
+        double azimuthCounts = angleMotor.getSelectedSensorPosition();
+        double radians = 2.0 * Math.PI * azimuthCounts / angleCountsPerRev;
+        return new Rotation2d(radians);
+    }
+
+    public void setAngleRotation2D(Rotation2d angle) {
+        setAngleOptimizedState(new SwerveModuleState(0.0, angle));
+    }
+
+    private SwerveModuleState setAngleOptimizedState(SwerveModuleState desiredState) {
+        // minimize change in heading by potentially reversing the drive direction
+        Rotation2d currentAngle = getAngleMotorRotation2d();
+        SwerveModuleState optimizedState = SwerveModuleState.optimize(desiredState, currentAngle);
+
+        // set the azimuth wheel position
+        double countsBefore = angleMotor.getSelectedSensorPosition();
+        double countsFromAngle = optimizedState.angle.getRadians() / (2.0 * Math.PI) * angleCountsPerRev;
+        double countsDelta = Math.IEEEremainder(countsFromAngle - countsBefore, angleCountsPerRev);
+        angleMotor.set(ControlMode.MotionMagic, countsBefore + countsDelta);
+
+        // save previous angle for use if inside deadband in setDesiredState()
+        previousAngle = optimizedState.angle;
+        return optimizedState;
+    }
+
+    private double getDriveMetersPerSecond() {
+        double encoderCountsPer100ms = driveMotor.getSelectedSensorVelocity();
+        double motorRotationsPer100ms = encoderCountsPer100ms / driveCountsPerRev;
+        double wheelRotationsPer100ms = motorRotationsPer100ms * driveGearRatio;
+        double metersPer100ms = wheelRotationsPer100ms * wheelCircumferenceMeters;
+        return metersPer100ms * k100msPerSecond;
+    }
+
+    private double getDrivePositionMeters() {
+        double encoderTicks = driveMotor.getSelectedSensorPosition();
+        double motorPositionTicks = encoderTicks / driveCountsPerRev;
+        double wheelPositionTicks = motorPositionTicks * driveGearRatio;
+        double wheelPositionMeters = wheelPositionTicks * wheelCircumferenceMeters;
+        return wheelPositionMeters;
+    }
+
+    private void setDriveOpenLoopMetersPerSecond(double metersPerSecond) {
+        driveMotor.set(ControlMode.PercentOutput, metersPerSecond / driveMaximumMetersPerSecond);
+    }
+
+    private void setDriveClosedLoopMetersPerSecond(double metersPerSecond) {
+        double wheelRotationsPerSecond = metersPerSecond / wheelCircumferenceMeters;
+        double motorRotationsPerSecond = wheelRotationsPerSecond / driveGearRatio;
+        double encoderCountsPerSecond = motorRotationsPerSecond * driveCountsPerRev;
+        driveMotor.set(ControlMode.Velocity, encoderCountsPerSecond / k100msPerSecond);
+    }
+
+    private int getWheelIndex() {
+        if (wheelLocationMeters.getX() > 0 && wheelLocationMeters.getY() > 0) { // Front Left
+            return 0;
+        }
+        if (wheelLocationMeters.getX() > 0 && wheelLocationMeters.getY() < 0) { // Front Right
+            return 1;
+        }
+        if (wheelLocationMeters.getX() < 0 && wheelLocationMeters.getY() > 0) { // Back Left
+            return 2;
+        }
+        return 3; // Back Right
+    }
+
+    @Override
+    public String toString() {
+        return "SwerveModule{" + getWheelIndex() + '}';
+    }
+
+}
