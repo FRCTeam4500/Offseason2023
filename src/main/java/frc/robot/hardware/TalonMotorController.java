549f337034c99e5fc9bc15f5fcb5ebc097a5c81f
Sal <spoovathumkadavil16@claytonschools.net>
2023-10-04 21:00:06 -0500

    change optimizer for modules
    source: https://www.chiefdelphi.com/t/swerve-modules-flip-180-degrees-periodically-conditionally/393059/4

--- src/main/java/frc/robot/hardware/TalonMotorController.java
+++ src/main/java/frc/robot/hardware/TalonMotorController.java
@@ -3,72 +3,104 @@ package frc.robot.hardware;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.can.BaseTalon;
-
 import frc.robot.Constants.EnumConstants.TalonType;
 import frc.robot.hardware.interfaces.SwerveMotorController;
 import frc.robot.subsystems.messaging.MessagingSystem;
 
-public class TalonMotorController extends BaseTalon implements SwerveMotorController{
-    private double TICKS_PER_RADIAN;
+public class TalonMotorController
+	extends BaseTalon
+	implements SwerveMotorController {
 
-    public TalonMotorController(int deviceID, TalonType type) {
-        super(deviceID, type.model);
-        switch (type.model) {
-            case "Talon FX":
-                TICKS_PER_RADIAN = 2048 / Math.PI / 2;
-                break;
-            case "Talon SRX": 
-                TICKS_PER_RADIAN = 4096 / Math.PI / 2;
-                break;
-            default:
-                MessagingSystem.getInstance().addMessage("Can ID #" + deviceID + "'s motor model isn't a valid option");
-        }
-    }
+	private double TICKS_PER_RADIAN;
 
-    public void setOutput(double targetPercentOutput) {
-        set(ControlMode.PercentOutput, targetPercentOutput);
-    }
+	public TalonMotorController(int deviceID, TalonType type) {
+		super(deviceID, type.model);
+		switch (type.model) {
+			case "Talon FX":
+				TICKS_PER_RADIAN = 2048 / Math.PI / 2;
+				break;
+			case "Talon SRX":
+				TICKS_PER_RADIAN = 4096 / Math.PI / 2;
+				break;
+			default:
+				MessagingSystem
+					.getInstance()
+					.addMessage(
+						"Can ID #" +
+						deviceID +
+						"'s motor model isn't a valid option"
+					);
+		}
+	}
 
-    public double getOutput() {
-        return getMotorOutputPercent();
-    }
+	public void setOutput(double targetPercentOutput) {
+		set(ControlMode.PercentOutput, targetPercentOutput);
+	}
 
-    public void setAngularVelocity(double targetAngularVelocity) {
-        set(ControlMode.Velocity, targetAngularVelocity * TICKS_PER_RADIAN / 10.0);
-    }
+	public double getOutput() {
+		return getMotorOutputPercent();
+	}
 
-    public double getAngularVelocity() {
-        return getSelectedSensorVelocity() / TICKS_PER_RADIAN * 10;
-    }
+	public void setAngularVelocity(double targetAngularVelocity) {
+		set(
+			ControlMode.Velocity,
+			targetAngularVelocity * TICKS_PER_RADIAN / 10.0
+		);
+	}
 
-    public void setAngle(double targetAngle) {
-        if (TICKS_PER_RADIAN == 4096 / Math.PI / 2) {
-            set(ControlMode.Position, targetAngle * TICKS_PER_RADIAN);
-        } else {
-            set(ControlMode.MotionMagic, targetAngle * TICKS_PER_RADIAN);
-        }
-    }
+	public double getAngularVelocity() {
+		return getSelectedSensorVelocity() / TICKS_PER_RADIAN * 10;
+	}
 
-    public double getAngle() {
-        return getSelectedSensorPosition() / TICKS_PER_RADIAN;
-    }
+	public void setAngle(double targetAngle) {
+		if (TICKS_PER_RADIAN == 4096 / Math.PI / 2) {
+			set(ControlMode.Position, targetAngle * TICKS_PER_RADIAN);
+		} else {
+			set(ControlMode.MotionMagic, targetAngle * TICKS_PER_RADIAN);
+		}
+	}
 
-    public void configureForSwerve(boolean isInverted, int currentLimit, double kP, double kD, boolean isDriveMotor){
-        if (isDriveMotor) {
-            configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, currentLimit, currentLimit + 1, 0.1),
-			50);
-            config_kP(0, kP);
-            config_kF(0, 0.047);
-            config_IntegralZone(0, 0);
-            setInverted(isInverted);
-        } else {
-            setInverted(isInverted);
-            config_kP(0, kP);
-            configMotionCruiseVelocity(10000);
-            configMotionAcceleration(10000);
-            configAllowableClosedloopError(0, 0);
-            configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, currentLimit, currentLimit + 1, 0.1), 50);
-            configClearPositionOnQuadIdx(true, 10);
-        }
-    }
+	public double getAngle() {
+		return getSelectedSensorPosition() / TICKS_PER_RADIAN;
+	}
+
+	public void configureForSwerve(
+		boolean isInverted,
+		int currentLimit,
+		double kP,
+		double kD,
+		boolean isDriveMotor
+	) {
+		if (isDriveMotor) {
+			configSupplyCurrentLimit(
+				new SupplyCurrentLimitConfiguration(
+					true,
+					currentLimit,
+					currentLimit + 1,
+					0.1
+				),
+				50
+			);
+			config_kP(0, kP);
+			config_kF(0, 0.047);
+			config_IntegralZone(0, 0);
+			setInverted(isInverted);
+		} else {
+			setInverted(isInverted);
+			config_kP(0, kP);
+			configMotionCruiseVelocity(10000);
+			configMotionAcceleration(10000);
+			configAllowableClosedloopError(0, 0);
+			configSupplyCurrentLimit(
+				new SupplyCurrentLimitConfiguration(
+					true,
+					currentLimit,
+					currentLimit + 1,
+					0.1
+				),
+				50
+			);
+			configClearPositionOnQuadIdx(true, 10);
+		}
+	}
 }
--- src/main/java/frc/robot/subsystems/swerve/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveModule.java
@@ -1,18 +1,18 @@
 package frc.robot.subsystems.swerve;
 
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import frc.robot.Constants.SwerveConstants;
 import frc.robot.Constants.EnumConstants.TalonType;
+import frc.robot.Constants.SwerveConstants;
 import frc.robot.hardware.SparkMaxMotorController;
 import frc.robot.hardware.TalonMotorController;
 import frc.robot.hardware.interfaces.SwerveMotorController;
 
 public class SwerveModule {
+
 	private SwerveMotorController driveMotor;
 	private SwerveMotorController angleMotor;
 	private Translation2d translationFromCenter;
@@ -39,13 +39,15 @@ public class SwerveModule {
 		boolean neoAngle
 	) {
 		if (neoDrive) {
-			driveMotor = new SparkMaxMotorController(driveId, MotorType.kBrushless);
+			driveMotor =
+				new SparkMaxMotorController(driveId, MotorType.kBrushless);
 		} else {
 			driveMotor = new TalonMotorController(driveId, TalonType.TalonFX);
 		}
 
 		if (neoAngle) {
-			angleMotor = new SparkMaxMotorController(angleId, MotorType.kBrushless);
+			angleMotor =
+				new SparkMaxMotorController(angleId, MotorType.kBrushless);
 		} else {
 			angleMotor = new TalonMotorController(angleId, TalonType.TalonFX);
 		}
@@ -56,7 +58,7 @@ public class SwerveModule {
 	}
 
 	public void drive(SwerveModuleState initialTargetState) {
-		SwerveModuleState targetState = SwerveModuleState.optimize(
+		SwerveModuleState targetState = TalonOptimizer.optimize(
 			initialTargetState,
 			getModuleState().angle
 		);
@@ -84,7 +86,7 @@ public class SwerveModule {
 	public SwerveModulePosition getModulePosition() {
 		return new SwerveModulePosition(
 			driveMotor.getAngle() /
-			(2 * Math.PI) * 
+			(2 * Math.PI) *
 			SwerveConstants.DRIVE_RATIO *
 			SwerveConstants.WHEEL_DIAMETER *
 			Math.PI,
@@ -102,8 +104,74 @@ public class SwerveModule {
 
 	public void setModuleVelocity(double targetVelocityMetersPerSecond) {
 		driveMotor.setAngularVelocity(
-			targetVelocityMetersPerSecond * 2 /
+			targetVelocityMetersPerSecond *
+			2 /
 			(SwerveConstants.DRIVE_RATIO * SwerveConstants.WHEEL_DIAMETER)
 		);
+	}
+
+	static class TalonOptimizer {
+
+		/**
+		 * Minimize the change in heading the desired swerve module state would require by potentially
+		 * reversing the direction the wheel spins. Customized from WPILib's version to include placing
+		 * in appropriate scope for CTRE onboard control.
+		 *
+		 * @param desiredState The desired state.
+		 * @param currentAngle The current module angle.
+		 */
+		public static SwerveModuleState optimize(
+			SwerveModuleState desiredState,
+			Rotation2d currentAngle
+		) {
+			double targetAngle = placeInAppropriate0To360Scope(
+				currentAngle.getDegrees(),
+				desiredState.angle.getDegrees()
+			);
+			double targetSpeed = desiredState.speedMetersPerSecond;
+			double delta = targetAngle - currentAngle.getDegrees();
+			if (Math.abs(delta) > 90) {
+				targetSpeed = -targetSpeed;
+				targetAngle =
+					delta > 90 ? (targetAngle -= 180) : (targetAngle += 180);
+			}
+			return new SwerveModuleState(
+				targetSpeed,
+				Rotation2d.fromDegrees(targetAngle)
+			);
+		}
+
+		/**
+		 * @param scopeReference Current Angle
+		 * @param newAngle Target Angle
+		 * @return Closest angle within scope
+		 */
+		private static double placeInAppropriate0To360Scope(
+			double scopeReference,
+			double newAngle
+		) {
+			double lowerBound;
+			double upperBound;
+			double lowerOffset = scopeReference % 360;
+			if (lowerOffset >= 0) {
+				lowerBound = scopeReference - lowerOffset;
+				upperBound = scopeReference + (360 - lowerOffset);
+			} else {
+				upperBound = scopeReference - lowerOffset;
+				lowerBound = scopeReference - (360 + lowerOffset);
+			}
+			while (newAngle < lowerBound) {
+				newAngle += 360;
+			}
+			while (newAngle > upperBound) {
+				newAngle -= 360;
+			}
+			if (newAngle - scopeReference > 180) {
+				newAngle -= 360;
+			} else if (newAngle - scopeReference < -180) {
+				newAngle += 360;
+			}
+			return newAngle;
+		}
 	}
 }
