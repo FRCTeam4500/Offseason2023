c4baf1ead083afd09b96861666b2013075038f49
TruestGecko <92557966+TruestGecko@users.noreply.github.com>
2023-10-04 23:02:07 -0500

    Added Balance Test

--- src/main/java/frc/robot/autonomous/Autonomous.java
+++ src/main/java/frc/robot/autonomous/Autonomous.java
@@ -3,8 +3,11 @@ package frc.robot.autonomous;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Constants.EnumConstants.ControlMode;
 import frc.robot.autonomous.autos.BalanceAuto;
 import frc.robot.autonomous.autos.OnePieceAuto;
+import frc.robot.commands.complexCommands.SwerveDriveCommand;
 import frc.robot.subsystems.placer.arm.Arm;
 import frc.robot.subsystems.placer.intake.Intake;
 import frc.robot.subsystems.swerve.SwerveDrive;
@@ -28,6 +31,7 @@ public class Autonomous {
 		autonChooser.setDefaultOption("No auto", null);
 		autonChooser.addOption("One Piece", new OnePieceAuto());
 		autonChooser.addOption("Balance", new BalanceAuto());
+		autonChooser.addOption("Balance Test", new InstantCommand(() -> SwerveDriveCommand.getInstance().setControlMode(ControlMode.Balance)));
 		Shuffleboard.getTab("Auto").add("Auto Routes", autonChooser);
 	}
 
